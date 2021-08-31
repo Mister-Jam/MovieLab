0a9012690cf6db43c9d551d885f8310f51b8f332
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-31 17:25:42 +0100

    add extensions for tableview

--- MovieLab/Extensions/UITableViewExtensions.swift
+++ MovieLab/Extensions/UITableViewExtensions.swift
@@ -5,4 +5,16 @@
 //  Created by King Bileygr on 8/31/21.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func registerTableViewCell(cell: UITableViewCell.Type) {
+        let identifier = String(describing: cell)
+        self.register(cell, forCellReuseIdentifier: identifier)
+    }
+    
+    func dequeueReusableCell(with cell: UITableViewCell.Type, indexPath: IndexPath) -> UITableViewCell {
+        let identifier = String(describing: cell)
+        return self.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    }
+}
