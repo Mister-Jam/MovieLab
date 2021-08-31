4e8656d32d5337ddb8eac879b0ccc7a7ae1b0179
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-31 17:05:56 +0100

    initial commit

---
+++ MovieLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLab
+//
+//  Created by King Bileygr on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.07450980392, green: 0.1764705882, blue: 0.3058823529, alpha: 1)
+    }
+
+
+}
+
