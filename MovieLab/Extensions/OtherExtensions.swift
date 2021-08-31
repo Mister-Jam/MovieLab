8ec57e9025c483a0725f0219f8d551fe7d2b16db
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-31 17:16:43 +0100

    create extension group and files

---
+++ MovieLab/Extensions/OtherExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  OtherExtensions.swift
+//  MovieLab
+//
+//  Created by King Bileygr on 8/31/21.
+//
+
+import Foundation
---
+++ MovieLab/Extensions/UICollectionViewExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  UICollectionViewExtensions.swift
+//  MovieLab
+//
+//  Created by King Bileygr on 8/31/21.
+//
+
+import Foundation
