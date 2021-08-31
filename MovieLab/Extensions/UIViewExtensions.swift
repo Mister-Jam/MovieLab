af01b46a4916c8db298b2d218193aace59e070d7
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-08-31 17:25:12 +0100

    add extensions for uiview

--- MovieLab/Extensions/UIViewExtensions.swift
+++ MovieLab/Extensions/UIViewExtensions.swift
@@ -5,4 +5,38 @@
 //  Created by King Bileygr on 8/31/21.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIView {
+    
+    var top: CGFloat {
+        self.frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        self.frame.size.height + top
+    }
+    
+    var left: CGFloat {
+        self.frame.origin.x
+    }
+    
+    var width: CGFloat {
+        self.frame.size.width
+    }
+    
+    var height: CGFloat {
+        self.frame.size.height
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    func addMultipleSub(views: [UIView] ) {
+        views.forEach { view in
+            self.addSubview(view)
+        }
+    }
+}
