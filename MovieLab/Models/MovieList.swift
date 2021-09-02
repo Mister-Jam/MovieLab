b6856d1474a59bd26b4ca7c6dfa82acd6a604dbf
Mister-Jam <73330887+Mister-Jam@users.noreply.github.com>
2021-09-02 11:14:20 +0100

    create realm object model

--- MovieLab/Models/MovieList.swift
+++ MovieLab/Models/MovieList.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import RealmSwift
+
+class MovieList: Object {
+    @Persisted(primaryKey: true) var id = ""
+    @Persisted var categoryName = ""
+    @Persisted var results: List<MovieListItems>
+}
+
+class MovieListItems: Object {
+    @Persisted var id = 0
+    @Persisted var title = ""
+    @Persisted var backdropPath = ""
+}
